5b0f9f590c6c710342b08bea597750ef803205b9
ArbuzovDmtr <arbuzov.dmtr@gmail.com>
2026-03-13 11:43:47 +0100

    made getProductById Optional

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,15 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
+
 import java.util.UUID;
 
 public class ShopService {
-    private ProductRepo productRepo = new ProductRepo();
-    private OrderRepo orderRepo = new OrderMapRepo();
+    private final ProductRepo productRepo = new ProductRepo();
+    private final OrderRepo orderRepo = new OrderMapRepo();
 
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
+            Product productToOrder = productRepo.getProductById(productId).orElse(null);
             if (productToOrder == null) {
                 System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
                 return null;
