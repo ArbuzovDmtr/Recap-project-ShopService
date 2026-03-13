a48c78e077b731b7ad23edc8fc0312b27a7fd44d
ArbuzovDmtr <arbuzov.dmtr@gmail.com>
2026-03-13 14:26:39 +0100

    fixed OrderListRepo and OrderMapRepo Tests

--- src/test/java/OrderListRepoTest.java
+++ src/test/java/OrderListRepoTest.java
@@ -23,7 +23,7 @@ class OrderListRepoTest {
         //THEN
         List<Order> expected = new ArrayList<>();
         Product product1 = new Product("1", "Apfel");
-        expected.add(new Order("1", List.of(product1),Status.PROCESSING, Instant.now()));
+        expected.add(new Order("1", List.of(product1),Status.PROCESSING, newOrder.timeBenchmark()));
 
         assertEquals(actual, expected);
     }
@@ -42,7 +42,7 @@ class OrderListRepoTest {
 
         //THEN
         Product product1 = new Product("1", "Apfel");
-        Order expected = new Order("1", List.of(product1),Status.PROCESSING,Instant.now());
+        Order expected = new Order("1", List.of(product1),Status.PROCESSING,newOrder.timeBenchmark());
 
         assertEquals(actual, expected);
     }
@@ -59,7 +59,7 @@ class OrderListRepoTest {
 
         //THEN
         Product product1 = new Product("1", "Apfel");
-        Order expected = new Order("1", List.of(product1),Status.PROCESSING,Instant.now());
+        Order expected = new Order("1", List.of(product1),Status.PROCESSING,newOrder.timeBenchmark());
         assertEquals(actual, expected);
         assertEquals(repo.getOrderById("1"), expected);
     }
