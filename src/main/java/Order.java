3d54409e11c11c974e1d33941227e6eed0b80260
ArbuzovDmtr <arbuzov.dmtr@gmail.com>
2026-03-13 10:06:12 +0100

    fixed constructor for Order in ShopService

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,5 +1,7 @@
 import java.util.List;
 enum Status {PROCESSING, IN_DELIVERY, COMPLETED}
+
+
 public record Order(
         String id,
         List<Product> products,
--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -17,7 +17,7 @@ public class ShopService {
             products.add(productToOrder);
         }
 
-        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+        Order newOrder = new Order(UUID.randomUUID().toString(), products, Status.COMPLETED);
 
         return orderRepo.addOrder(newOrder);
     }
